178ddae088673b5ed0cbe7fbff4ee7caa7a21a80
github username <github email address>
2019-01-30 09:23:56 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,54 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  require 'pry'
+
+  #welcome
+  get '/' do
+    erb :welcome
+  end
+
+  #index
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  #new
+  get '/recipes/new' do
+    erb :new
+  end
+
+  #create
+  post '/recipes' do
+    recipe = Recipe.create(params[:recipe])
+    redirect "/recipes/#{recipe.id}"
+  end
+
+  #show
+  get '/recipes/:id' do
+    @recipes = Recipe.find(params[:id])
+    erb :show
+  end
+
+  #edit
+  get '/recipes/:id/edit' do
+
+    @recipes = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  #Update
+  patch '/recipes/:id' do
+    recipe = Recipe.find(params[:id])
+    recipe.update(params[:recipe])
+    redirect "/recipes/#{recipe.id}"
+  end
+
+  #Destroy
+  delete '/recipes/:id/delete' do
+    Recipe.find(params[:id]).destroy
+    redirect "/recipes"
+  end
 
 end
